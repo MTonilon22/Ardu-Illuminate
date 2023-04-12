1e3a3397b40b600f402659759baf3985fcdcb600
ampats04 <jeremyandyampatin@gmail.com>
2023-04-12 16:14:59 +0800

    Added Additional dependencies, connected esp3266 with flutter

--- lib/third.dart
+++ lib/third.dart
@@ -9,9 +9,61 @@ class ThirdScreen extends StatefulWidget {
 }
 
 class _ThirdScreenState extends State<ThirdScreen> {
-  bool light1 = true;
+  bool light1 = false;
   late Color activeColor = Colors.green;
   double _currentSliderValue = 20;
+  bool ledstatus = false;
+  late IOWebSocketChannel channel;
+  bool connected = false;
+
+  void initstate() {
+    Future.delayed(Duration.zero, () async {
+      channelconnect();
+    });
+
+    super.initState();
+  }
+
+  channelconnect() {
+    try {
+      channel =
+          IOWebSocketChannel.connect("ws://192.168.0.1:81"); //channel IP : Port
+      channel.stream.listen(
+        (message) {
+          print(message);
+          setState(() {
+            if (message == "connected") {
+              connected = true; //message is "connected" from NodeMCU
+            } else if (message == "poweron:success") {
+              ledstatus = true;
+            } else if (message == "poweroff:success") {
+              ledstatus = false;
+            }
+          });
+        },
+        onDone: () {
+          //if WebSocket is disconnected
+          setState(() {
+            connected = false;
+          });
+        },
+        onError: (error) {},
+      );
+    } catch (_) {}
+  }
+
+  Future<void> sendcmd(String cmd) async {
+    if (connected == true) {
+      if (ledstatus == false && cmd != "poweron" && cmd != "poweroff") {
+        print("Send the valid command");
+      } else {
+        channel.sink.add(cmd); //sending Command to NodeMCU
+      }
+    } else {
+      channelconnect();
+      print("Websocket is not connected.");
+    }
+  }
 
   final MaterialStateProperty<Icon?> thumbIcon =
       MaterialStateProperty.resolveWith<Icon?>(
@@ -24,6 +76,13 @@ class _ThirdScreenState extends State<ThirdScreen> {
   );
 
   void _onChanged(bool value) {
+    if (ledstatus) {
+      sendcmd("poweroff");
+      ledstatus = false;
+    } else {
+      sendcmd("poweron");
+      ledstatus = true;
+    }
     setState(() {
       light1 = value;
       activeColor = value ? Colors.green : Colors.red;
