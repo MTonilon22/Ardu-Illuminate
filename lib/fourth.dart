6fac7328fc1785674712a9f03132d544d9d73379
MTonilon22 <mtonilon17@gmail.com>
2023-04-13 18:55:44 +0800

    added Timer Logic, incomplete

--- lib/fourth.dart
+++ lib/fourth.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'third.dart';
 
 class FourthScreen extends StatefulWidget {
   const FourthScreen({Key? key}) : super(key: key);
@@ -10,7 +14,43 @@ class FourthScreen extends StatefulWidget {
 
 class _FourthScreenState extends State<FourthScreen> {
   TimeOfDay _time = TimeOfDay.now();
-  late TimeOfDay picked;
+  TimeOfDay _timee = TimeOfDay.now();
+  TimeOfDay? picked;
+  DateTime? parsedTime;
+  Timer? _timer;
+  int oten = 30;
+  Duration? myDuration;
+  var format = DateFormat("HH:mm:ss");
+
+  void startTimer() {
+    myDuration = Duration(days: 10);
+    _timer = Timer.periodic(const Duration(seconds: 1), (_) => setCountDown());
+    print(myDuration);
+  }
+
+  void stopTimer() {
+    setState(() => _timer!.cancel());
+  }
+
+  void resetTimer() {
+    stopTimer();
+    setState(() => myDuration = Duration(hours: _time.hour - _timee.hour));
+  }
+
+  void setCountDown() {
+    // ignore: prefer_const_declarations
+    final reduceSecondsby = 1;
+
+    setState(() {
+      final seconds = myDuration.inSeconds - reduceSecondsby;
+
+      if (seconds < 0) {
+        _timer!.cancel();
+      } else {
+        myDuration = Duration(seconds: seconds);
+      }
+    });
+  }
 
   Future<void> selectTime(BuildContext context) async {
     picked = (await showTimePicker(
@@ -19,12 +59,20 @@ class _FourthScreenState extends State<FourthScreen> {
     ))!;
 
     setState(() {
-      _time = picked;
+      _time = picked!;
+      print(TimeOfDay.now());
+      print(_time);
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    String strDigits(int n) => n.toString().padLeft(2, '0');
+    final days = strDigits(myDuration.inDays);
+    final hours = strDigits(myDuration.inHours.remainder(24));
+    final minutes = strDigits(myDuration.inMinutes.remainder(60));
+    final seconds = strDigits(myDuration.inSeconds.remainder(60));
+
     return Expanded(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -33,6 +81,7 @@ class _FourthScreenState extends State<FourthScreen> {
             'Timer',
             textAlign: TextAlign.left,
           ),
+          Text('$hours:$minutes:$seconds'),
           IconButton(
             iconSize: 80,
             icon: const Icon(
@@ -43,6 +92,38 @@ class _FourthScreenState extends State<FourthScreen> {
               selectTime(context);
             },
           ),
+          ElevatedButton(
+            onPressed: startTimer,
+            child: Text(
+              'Start',
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              if (_timer == null || _timer!.isActive) {
+                stopTimer();
+              }
+            },
+            child: Text(
+              'Stop',
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                resetTimer();
+              },
+              child: Text(
+                'Reset',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              )),
           const SizedBox(
             height: 60,
           ),
